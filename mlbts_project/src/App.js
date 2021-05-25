44c6534919eea04e2882883799c603ace196999a
NuDrake <drakehubbard2020@u.northwestern.edu>
2021-05-25 15:22:15 -0500

    should show listing.

--- mlbts_project/src/App.js
+++ mlbts_project/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Heroku!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [{headerName: "Name", field: "name"}, 
+                    {headerName: "Best Sell Price", field: "best_sell_price"}, 
+                    {headerName: "Best Buy Price", field: "best_buy_price"}],
+      rowData: []
+    }
+  }
+  getListings() {
+    axios.get('https://cors-anywhere.herokuapp.com/https://mlb19.theshownation.com/apis/listings.json')
+      .then((response) => {
+        this.setState({
+          rowData: response.data
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount () {
+    this.getListings();
+  }
+
+  render() {
+    return (
+      <div className="ag-theme-balham">
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData.listings}>
+        </AgGridReact>
+      </div>
+    );
+  }
 }
 
 export default App;
